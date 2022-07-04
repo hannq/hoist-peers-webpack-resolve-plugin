da9c700b130d126a4df1cd088a889ac7d8e7a362
OF3808 <hannianqi@qianmi.com>
2022-07-04 09:48:29 +0800

    fix(symbolLinks): fix symbolLinks not work

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+import path from "path";
 import type { ResolvePluginInstance, Resolver } from "webpack";
 
 export interface Options {
@@ -11,18 +13,35 @@ export class HoistPeersWebpackResolvePlugin implements ResolvePluginInstance {
   constructor(private options: Options) {}
 
   apply(resolver: Resolver) {
+    const { options: { symlinks, modules, roots } } = resolver;
     const { deps = [] } = this.options;
-    const ignoreList = deps.map(depName => [
-      depName,
-      Object
-        .keys(require(`${depName}/package.json`)?.peerDependencies || {})
-        .map(peerName => `node_modules/${depName}/node_modules/${peerName}`)
-    ] as const);
-    resolver.getHook('existingFile').tapAsync(this.name, function(request, resolveContext, callback) {
+    const projectRoot = Array.from(roots)[0];
+    const nodeModulesBasePath = modules.find(module => typeof module === 'string' && path.isAbsolute(module) && module.endsWith('node_modules')) as string | null;
+    if (!nodeModulesBasePath || !nodeModulesBasePath.startsWith(projectRoot)) throw new Error(`valid node_modules not found !`);
+
+    const ignoreList = deps.map(depName => {
+      const expectPkgJsonPath = path.join(nodeModulesBasePath, `${depName}/package.json`);
+      const expectPkgDir = path.dirname(expectPkgJsonPath);
+      const realPkgDir = path.dirname(require.resolve(expectPkgJsonPath));
+      const isSymlink = symlinks && expectPkgDir !== realPkgDir;
+
+      return {
+        depName,
+        isSymlink,
+        expectPkgDir,
+        realPkgDir,
+        peers: Object
+          .keys(require(expectPkgJsonPath)?.peerDependencies || {})
+          .map(peerName => path.join(isSymlink ? realPkgDir : expectPkgDir, `node_modules/${peerName}`))
+      } as const
+    });
+    resolver.getHook('existingFile').tapAsync(this.name, function(request, _resolveContext, callback) {
       try {
-        const matchedDep = ignoreList.find(([, peers]) => peers.some(peer => request.path && request.path.includes(peer)));
+        const matchedDep = ignoreList.find(item => item.peers.some(peer => request.path && request.path.startsWith(peer)));
         if (matchedDep) {
-          request.path = (request.path as string).replace(`/${matchedDep[0]}/node_modules`, '');
+          const { isSymlink, expectPkgDir, realPkgDir } = matchedDep;
+          const rawRequestPath = request.path as string;
+          request.path = rawRequestPath.replace(path.join(isSymlink ? realPkgDir : expectPkgDir, 'node_modules'), nodeModulesBasePath);
         }
         callback();
       } catch (err) {
